317dc6a0e5fd0fc725295897a2d5c97a6a1d1541
Edwards Moses <edwardsmoses3@gmail.com>
2021-03-08 00:58:04 +0100

    Completed Header Design

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,10 @@
-import React from "react"
-import { Link, useStaticQuery, graphql } from "gatsby"
-import Navigation from "../components/navigation"
-import 'prismjs/themes/prism-okaidia.css';
+import React from "react";
+import { Link, useStaticQuery, graphql } from "gatsby";
 
-export default ({ children }) => {
+import Navigation from "../components/navigation";
+import "prismjs/themes/prism-okaidia.css";
+
+export default ({ children, header }) => {
   const data = useStaticQuery(
     graphql`
       query {
@@ -14,19 +15,32 @@ export default ({ children }) => {
         }
       }
     `
-  )
+  );
   return (
-    <div className="site-wrapper">
-      <header className="site-header">
-        <div className="site-title">
-          <Link to="/">{data.site.siteMetadata.title}</Link>
-        </div>
-        <Navigation />
-      </header>
-      {children}
-      <footer className="site-footer">
-        <p>&copy; {new Date().getFullYear()} &bull; Edwards Moses &bull; Crafted with <span role="img" aria-label="love">❤️</span></p>
-      </footer>
-    </div>
-  )
-}
+    <>
+      <div className="site-wrapper topSiteWrapper">
+        <header className="site-header">
+          <div className="site-title">
+            <Link to="/">{data.site.siteMetadata.title}</Link>
+          </div>
+          <Navigation />
+        </header>
+      </div>
+
+      {header ? <div>{header}</div> : null}
+
+      <div className="site-wrapper">
+        {children}
+        <footer className="site-footer">
+          <p>
+            &copy; {new Date().getFullYear()} &bull; Edwards Moses &bull;
+            Crafted with{" "}
+            <span role="img" aria-label="love">
+              ❤️
+            </span>
+          </p>
+        </footer>
+      </div>
+    </>
+  );
+};
