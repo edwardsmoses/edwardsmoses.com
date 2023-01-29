94b31bf71c9bb1382fcb082010f73be7723d4aa9
Edwards Moses <edwardsmoses3@gmail.com>
2023-01-29 10:50:06 +0100

    feat: add my interview with the Gosolo team to my website.

---
+++ _data/blog/2023-01-29-interview-with-gosolo.md
@@ -0,0 +1,12 @@
+---
+template: BlogPost
+path: /interview-with-go-solo
+externalLink: https://gosolo.subkit.com/edwards-moses/
+date: 2023-01-29T10:24:36.203Z
+title: Interview with the GoSolo Subkit team
+thumbnail: /assets/gosolo-edwards-moses-interview.jpg
+---
+
+<!--StartFragment-->
+
+<!--EndFragment-->
--- src/components/post-link.js
+++ src/components/post-link.js
@@ -1,26 +1,54 @@
 import React from "react";
 import { Link } from "gatsby";
 
+const PostLinkImage = ({ post }) => {
+  return (
+    <div className="flex-shrink-0">
+      <img
+        className="h-48 w-full object-cover"
+        src={post.frontmatter.thumbnail}
+        alt={post.frontmatter.title + "- Featured Shot"}
+      />
+    </div>
+  );
+};
+
+const PostLinkHeader = ({ post }) => {
+  return (
+    <p className="block mt-2">
+      <p className="text-xl font-semibold">{post.frontmatter.title}</p>
+    </p>
+  );
+};
+
 const PostLink = ({ post }) => (
   <>
     <article className="flex flex-col rounded-lg shadow-lg overflow-hidden">
-      <Link to={post.frontmatter.path}>
-        <div className="flex-shrink-0">
-          <img
-            className="h-48 w-full object-cover"
-            src={post.frontmatter.thumbnail}
-            alt={post.frontmatter.title + "- Featured Shot"}
-          />
-        </div>
-      </Link>
+      {post.frontmatter.externalLink ? (
+        <a href={post.frontmatter.externalLink} target="blank">
+          <PostLinkImage post={post} />
+        </a>
+      ) : (
+        <Link to={post.frontmatter.path}>
+          <PostLinkImage post={post} />
+        </Link>
+      )}
 
       <div className="flex-1 bg-white p-6 flex flex-col justify-between">
         <div className="flex-1">
-          <Link to={post.frontmatter.path} className="text-gray-900 hover:no-underline hover:text-app-brand-yellow">
-            <p className="block mt-2">
-              <p className="text-xl font-semibold">{post.frontmatter.title}</p>
-            </p>
-          </Link>
+          {post.frontmatter.externalLink ? (
+            <a
+              href={post.frontmatter.externalLink}
+              target="blank"
+              className="text-gray-900 hover:no-underline hover:text-app-brand-yellow"
+            >
+              <PostLinkHeader post={post} />
+            </a>
+          ) : (
+            <Link to={post.frontmatter.path} className="text-gray-900 hover:no-underline hover:text-app-brand-yellow">
+              <PostLinkHeader post={post} />
+            </Link>
+          )}
         </div>
         <div className="mt-3 flex items-center">
           <div>
@@ -33,4 +61,5 @@ const PostLink = ({ post }) => (
     </article>
   </>
 );
+
 export default PostLink;
