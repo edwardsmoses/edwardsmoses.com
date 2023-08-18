df1b144a86559a5133db433cb50eb5126d7f9b86
Edwards Moses <edwardsmoses3@gmail.com>
2023-08-18 14:33:28 +0100

    feat: add Book Call button to the top of page

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -3,9 +3,17 @@ import { AnchorLink } from "gatsby-plugin-anchor-links";
 
 export default (props) => (
   <nav className="navigation">
+
+    <AnchorLink to="/#articles">
+      <a href="#" className="font-medium border-b-2 dark:border-white border-black">
+        Articles
+      </a>
+    </AnchorLink>
+
     <a href="/about" className="font-medium">
       About
     </a>
+
     <a href="/projects" className="font-medium">
       Projects
     </a>
@@ -13,10 +21,11 @@ export default (props) => (
       Contact
     </a>
 
-    <AnchorLink to="/#articles" title="Read My Blog">
-      <button className="inline-flex justify-center rounded-sm text-sm font-semibold py-2 px-4 text-white bg-app-brand -my-2.5 ml-5">
-        Articles
-      </button>
-    </AnchorLink>
+    <button onClick={() => {
+      window.open("https://calendly.com/edwardsmoses/30min", "_blank")
+    }} className="inline-flex justify-center rounded-sm text-sm font-semibold py-2 px-4 text-white bg-app-brand -my-2.5 ml-5">
+      Book a call
+    </button>
+
   </nav>
 );
