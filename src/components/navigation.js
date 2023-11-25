be9d3f4f59b6d494ff35e71396e13c1f143699cb
Edwards Moses <edwardsmoses3@gmail.com>
2023-11-25 23:24:03 +0100

    feat(ui): update text wrap styling on mobile for navigation links

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,19 +4,19 @@ import { Link } from "gatsby";
 
 export default (props) => (
   <nav className="navigation">
-    <Link to="/articles" className="font-medium" activeClassName="font-bold !text-app-black dark:!text-white">
+    <Link to="/articles" className="font-medium text-xs lg:text-base whitespace-nowrap" activeClassName="font-bold !text-app-black dark:!text-white">
       Articles
     </Link>
 
-    <Link to="/about" className="font-medium" activeClassName="font-bold !text-app-black dark:!text-white">
+    <Link to="/about" className="font-medium text-xs lg:text-base whitespace-nowrap" activeClassName="font-bold !text-app-black dark:!text-white">
       About me
     </Link>
 
-    <OutboundLink href="https://portfolio.edwardsmoses.com/#projects" className="font-medium" target="_blank">
+    <OutboundLink href="https://portfolio.edwardsmoses.com/#projects" className="text-xs lg:text-base whitespace-nowrap font-medium" target="_blank">
       My Portfolio
     </OutboundLink>
 
-    <OutboundLink href="https://calendly.com/edwardsmoses/30min" target="_blank" className="inline-flex justify-center rounded-sm text-sm font-semibold py-2 px-4 !text-app-brand-white bg-app-brand dark:bg-app-brand-white dark:!text-black -my-2.5 ml-5">
+    <OutboundLink href="https://calendly.com/edwardsmoses/30min" target="_blank" className="text-xs whitespace-nowrap lg:text-base inline-flex justify-center rounded-sm font-semibold py-2 px-4 !text-app-brand-white bg-app-brand dark:bg-app-brand-white dark:!text-black -my-2.5 ml-5">
       Let's chat
     </OutboundLink>
 
