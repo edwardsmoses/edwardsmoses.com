d34a74814c445dc236e511e9bf7ddb70b34f23a9
Edwards Moses <edwardsmoses3@gmail.com>
2023-11-25 21:34:50 +0100

    feat: update copy in header navigation

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -13,7 +13,7 @@ export default (props) => (
     </Link>
 
     <OutboundLink href="https://portfolio.edwardsmoses.com/#projects" className="font-medium" target="_blank">
-      Projects
+      Portfolio
     </OutboundLink>
 
     <OutboundLink href="https://calendly.com/edwardsmoses/30min" target="_blank" className="inline-flex justify-center rounded-sm text-sm font-semibold py-2 px-4 !text-white bg-app-brand -my-2.5 ml-5">
