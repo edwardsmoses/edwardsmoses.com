b18bbf711492dea0b8905d2c1821170bd7cd4d81
Edwards Moses <edwardsmoses3@gmail.com>
2024-04-09 19:17:44 +0100

    feat: add codecrafters profile to the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -13,7 +13,7 @@ const Footer = () => {
             className="text-gray-400 dark:text-white hover:text-gray-500"
           >
             <span className="sr-only">Linkedin</span>
-            <svg viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6 transition ">
+            <svg viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 transition ">
               <path d="M18.335 18.339H15.67v-4.177c0-.996-.02-2.278-1.39-2.278-1.389 0-1.601 1.084-1.601 2.205v4.25h-2.666V9.75h2.56v1.17h.035c.358-.674 1.228-1.387 2.528-1.387 2.7 0 3.2 1.778 3.2 4.091v4.715zM7.003 8.575a1.546 1.546 0 01-1.548-1.549 1.548 1.548 0 111.547 1.549zm1.336 9.764H5.666V9.75H8.34v8.589zM19.67 3H4.329C3.593 3 3 3.58 3 4.297v15.406C3 20.42 3.594 21 4.328 21h15.338C20.4 21 21 20.42 21 19.703V4.297C21 3.58 20.4 3 19.666 3h.003z"></path>
             </svg>
           </a>
@@ -25,7 +25,7 @@ const Footer = () => {
           >
             <span className="sr-only">GitHub</span>
 
-            <svg viewBox="0 0 24 24" aria-hidden="true" fill="currentColor" className="h-6 w-6 transition">
+            <svg viewBox="0 0 24 24" aria-hidden="true" fill="currentColor" className="w-6 h-6 transition">
               <path
                 fillRule="evenodd"
                 clipRule="evenodd"
@@ -41,7 +41,7 @@ const Footer = () => {
           >
             <span className="sr-only">Twitter</span>
 
-            <svg viewBox="0 0 24 24" aria-hidden="true" fill="currentColor" className="h-6 w-6  transition">
+            <svg viewBox="0 0 24 24" aria-hidden="true" fill="currentColor" className="w-6 h-6 transition">
               <path d="M20.055 7.983c.011.174.011.347.011.523 0 5.338-3.92 11.494-11.09 11.494v-.003A10.755 10.755 0 0 1 3 18.186c.308.038.618.057.928.058a7.655 7.655 0 0 0 4.841-1.733c-1.668-.032-3.13-1.16-3.642-2.805a3.753 3.753 0 0 0 1.76-.07C5.07 13.256 3.76 11.6 3.76 9.676v-.05a3.77 3.77 0 0 0 1.77.505C3.816 8.945 3.288 6.583 4.322 4.737c1.98 2.524 4.9 4.058 8.034 4.22a4.137 4.137 0 0 1 1.128-3.86A3.807 3.807 0 0 1 19 5.274a7.657 7.657 0 0 0 2.475-.98c-.29.934-.9 1.729-1.713 2.233A7.54 7.54 0 0 0 22 5.89a8.084 8.084 0 0 1-1.945 2.093Z"></path>
             </svg>
           </a>
@@ -201,7 +201,18 @@ const Footer = () => {
             <span className="sr-only">Duolingo</span>
 
             
-            <img src="/assets/duolingo.svg" alt="" className="h-6 w-6" />
+            <img src="/assets/duolingo.svg" alt="" className="w-6 h-6" />
+          </a>
+
+          <a
+            href="https://app.codecrafters.io/users/edwardsmoses"
+            target="_blank"
+            className="text-gray-400 dark:text-white hover:text-gray-500"
+          >
+            <span className="sr-only">CodeCrafters</span>
+
+            
+            <img src="https://app.codecrafters.io/assets/7408d202b2bb110054fc.svg" alt="" className="w-6 h-6" />
           </a>
 
 
@@ -215,7 +226,7 @@ const Footer = () => {
             <p className="text-base font-light text-center text-gray-400">View Source</p>
           </OutboundLink>
         </div>
-        <div className="mt-8 md:mt-0 md:order-1 flex flex-row space-x-3">
+        <div className="flex flex-row mt-8 space-x-3 md:mt-0 md:order-1">
         <ThemeChanger />
           <p className="text-base text-center text-gray-400">
             Edwards Moses <span>©</span> {new Date().getFullYear()}
