16da0d7ca9cdfee776735c694d8a930bbddb733d
Edwards Moses <edwardsmoses3@gmail.com>
2026-02-17 23:38:17 +0100

    feat: add new article to my list of articles

--- src/data/blogroll-feed.js
+++ src/data/blogroll-feed.js
@@ -1,5 +1,12 @@
 export const BLOGROLL_FEED_ENTRIES = [
   {
+    url: "https://allenpike.com/2026/a-broken-heart/?utm_source=bonobopress&utm_medium=newsletter&utm_campaign=2215",
+    publishedAt: "2026-02-16",
+    thoughts:
+      "this was a really good read.. pretty easy to follow.. aside from the shared downsides and upsides of using coding agents.. also shared some insights in using perfomance inspectors on a mac, which is something i plan on using straight away on a perf problem i have on a project at work..",
+  },
+
+  {
     url: "https://www.bitsaboutmoney.com/archive/fraud-investigation/",
     publishedAt: "2026-02-14",
     thoughts:
