1fb7a34c1e9787890f24a519f451f2e4fda5e1a2
Edwards Moses <edwardsmoses3@gmail.com>
2026-03-06 01:33:21 +0100

    feat: removing params

--- src/data/blogroll-feed.js
+++ src/data/blogroll-feed.js
@@ -1,12 +1,12 @@
 export const BLOGROLL_FEED_ENTRIES = [
   {
-    url: 'https://mollyg.substack.com/p/six-rules-for-company-goal-setting?utm_source=post-email-title&publication_id=701791&post_id=189687724&utm_campaign=email-post-title&isFreemail=true&r=9t7xv&triedRedirect=true&utm_medium=email',
+    url: 'https://mollyg.substack.com/p/six-rules-for-company-goal-setting',
     publishedAt: '2026-03-04',
     thoughts:
       "this article is interesting because it drives home the importance of ruthless goal setting in driving company success, alignment is always key",
   },
   {
-    url: "https://allenpike.com/2026/a-broken-heart/?utm_source=bonobopress&utm_medium=newsletter&utm_campaign=2215",
+    url: "https://allenpike.com/2026/a-broken-heart",
     publishedAt: "2026-02-16",
     thoughts:
       "this was a really good read.. pretty easy to follow.. aside from the shared downsides and upsides of using coding agents.. also shared some insights in using perfomance inspectors on a mac, which is something i plan on using straight away on a perf problem i have on a project at work..",
